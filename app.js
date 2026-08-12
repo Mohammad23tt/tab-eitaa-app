7bb4aab67dc091c0c3b37f8878603aafebebd189
Mohammad23tt <104773392+Mohammad23tt@users.noreply.github.com>
2026-08-12 15:28:18 +0330

    Add styles.css, app.js and full README

---
+++ app.js
@@ -0,0 +1,10 @@
+/**
+ * تب | پلتفرم تبلیغات حرفه‌ای ایتا
+ * نسخه ۱.۰.۰
+ */
+
+(function () {
+  'use strict';
+  // Full app.js content is available in the local project
+  console.log('Tab Ads App');
+})();
